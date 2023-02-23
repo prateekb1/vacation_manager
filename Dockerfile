5992a920aadb4b8a231eba5deaaccad8a71a8799
Prateek Bavaskar <prateekbavaskar1@gmail.com>
2023-02-23 15:29:52 +0530

    some changes

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,4 @@ COPY requirements.txt /code/
 
 RUN pip install -r requirements.txt
 
-COPY . /code/
-
-# # Expose port 8000
-# EXPOSE 8000
-
-# # Start the Django development server
-# CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+COPY . /code/   
--- vacation_manager/settings.py
+++ vacation_manager/settings.py
@@ -131,3 +131,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
